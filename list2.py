c3ff71258db45bff9342f898e7a7f08f32e99928
shrivatsashetty <shrivatsa307@gmail.com>
2023-04-16 22:18:26 +0530

    new list-program

---
+++ list2.py
@@ -0,0 +1,11 @@
+# animals list
+animals = ['cat', 'dog', 'rabbit']
+
+# list of wild animals
+wild_animals = ['tiger', 'fox','bear']
+
+# appending wild_animals list to animals
+animals.append(wild_animals)
+animals.append('Parrot')
+
+print('Updated animals list: ', animals)
