266bc436e2748320e2328ea580c6f47246f23a17
shrivatsashetty <shrivatsa307@gmail.com>
2023-05-01 13:47:26 +0530

    arbitrary args

---
+++ arbitraryargs.py
@@ -0,0 +1,5 @@
+def operations(*numbers): 
+    # a tuple numbers is created and arg are passed into it
+    print(f"the sum of {numbers[0]}+{numbers[1]} is {numbers[0]+numbers[1]}")
+
+operations(1,2,3,4) # calling the function with multiple args
---
+++ fnloop.py
@@ -0,0 +1,7 @@
+def list_out(list):
+    print("Fruits in your bucket are:")
+    for items in list:
+        print(items)
+
+fruits=["apple", "Banana", "Cherry"] 
+list_out(fruits)
