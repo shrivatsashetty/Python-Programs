b2d4772c636b714a2caa7c7818c805d628df6d39
shrivatsashetty <shrivatsa307@gmail.com>
2023-06-23 18:37:06 +0530

    practice folder

---
+++ Pytutors/np_array_intrinsic.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# intrinsic methods of creating numpy arrays
+
+zeroarray = np.zeros([2,3]) # creates 2 X 3 array containing zeroes
+print(zeroarray)
+
+rng1 = np.arange(8)
+print(rng1) # [0 1 2 3 4 5 6 7]
+
+rng2  = np.arange(4,10)
+print(rng2) # [4 5 6 7 8 9]
+
+rngreverse = np.arange(15,3,-3)
+print(rngreverse) # [15 12  9  6]
---
+++ Pytutors/numpy_intro.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+# creating numpy array using various methods
+myarray = np.array([[1,2,3,4],[5,6,7,8]]) # 2D array [[]]
+print(myarray)
+print(type(myarray)) # <class 'numpy.ndarray'>
+print(myarray.shape) # (2, 4)
+print(myarray.dtype) # int64
+print(myarray.size)  # 8
+print(myarray[1,2]) # acessing element in  2nd row third column
+myarray[1,2] = 5    # numpy arrays are mutable
+print(myarray[1,2]) # 5
+
+decimals = np.array([[1.3, 3.2, 7.3],[2.2,4.5,6.0]])
+print(decimals.dtype) # float64
+
+letters = np.array("R,V,C,E")
+print(letters)
+print(type(letters))
+print(letters.dtype)
+
+mynums = np.array(range(2,14,3))
+print(mynums)
+print(mynums.dtype) # int64
+
+rngnums = np.array(range(20,4,-4)) # [20 16 12  8]
+print(rngnums)
+
+setarray = np.array({2,"hi",4,6})
+print(setarray)
+print(type(setarray))
+print(setarray.dtype)
