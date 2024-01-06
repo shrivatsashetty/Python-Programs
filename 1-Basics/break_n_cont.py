20cc84de01f7f71e1c74415b63f4e9080cfa8467
shrivatsashetty <shrivatsa307@gmail.com>
2024-01-06 23:38:58 +0530

    corrected indent error

--- 1-Basics/break_n_cont.py
+++ 1-Basics/break_n_cont.py
@@ -1,5 +1,7 @@
 names=["Anand","Mohan","Sameer","Brendon","Mike","Jeevan"]
 for person in names :
     if person == "Brendon":
-    	break
-    print(person)
+        break
+    else:
+        print(person)
+
